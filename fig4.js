7f1fcefbc26cf24050dd208def2ee9053874c024
Jonas Almeida <jalmeida@mathbiol.org>
2016-02-11 15:26:01 -0500

    Fig4.1,2,3

--- fig4.js
+++ fig4.js
@@ -1,1 +1,9 @@
 console.log('fig4.js loaded')
+
+
+window.onload=function(){ // to avoid poluting the DOM
+    var h ='<table><tr><td id="fig4.1">Fig1.1</td></tr>'
+    h +='<tr><td id="fig4_1">Fig4.2</td></tr>'
+    h +='<tr><td id="fig4_1">Fig4.3</td></tr></table>'
+    fig4div.innerHTML=h
+}
