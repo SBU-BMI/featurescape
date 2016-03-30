8ec1b4a2a0c978e32f7722e306ccf1813b422cba
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2016-03-30 12:53:59 -0400

    change port
    Per Tahsin - 4500.

--- fun/config.js
+++ fun/config.js
@@ -5,6 +5,6 @@ var config = {
     //domain: 'localhost:63342',
     findAPI: '129.49.249.175',
     //findAPI: '129.49.249.191',
-    port: 3000,
+    port: 4500,
     analysis_execution_id: 'lung-features-v4'
 };
